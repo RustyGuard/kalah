3f8e515888398d90bbe2467adbf3d1f58b5dc82c
RustyGuard <golovinartem2004@gmail.com>
2025-04-05 11:32:16 +0400

    Authorization fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,43 @@
 import pytest
+from fastapi import status
 from fastapi.testclient import TestClient
 
 from main import app
 
 
 @pytest.fixture
-def test_client():
+def player_client():
     yield TestClient(app)
+
+
+@pytest.fixture
+def player_token(player_client):
+    response = player_client.post(
+        "/auth",
+        data={
+            "user_name": "Player",
+            "avatar_id": 1,
+        },
+        follow_redirects=False,
+    )
+    assert response.status_code == status.HTTP_302_FOUND
+    return response.cookies["access_token"]
+
+
+@pytest.fixture
+def opponent_client():
+    yield TestClient(app)
+
+
+@pytest.fixture
+def opponent_token(opponent_client):
+    response = opponent_client.post(
+        "/auth",
+        data={
+            "user_name": "Opponent",
+            "avatar_id": 2,
+        },
+        follow_redirects=False,
+    )
+    assert response.status_code == status.HTTP_302_FOUND
+    return response.cookies["access_token"]
--- tests/test_auth_required.py
+++ tests/test_auth_required.py
@@ -15,6 +15,26 @@ from fastapi.testclient import TestClient
         "/game_board",
     ],
 )
-def test_auth_redirect(test_client: TestClient, page_path: str):
-    response = test_client.get(page_path, follow_redirects=False)
+def test_auth_redirect(player_client: TestClient, page_path: str):
+    """Not authorized users should be redirected to `/auth` page"""
+    response = player_client.get(page_path, follow_redirects=False)
     assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
+
+
+@pytest.mark.parametrize(
+    "page_path",
+    [
+        "/",
+        "/settings",
+        "/help",
+        "/join_game",
+        "/lobby_settings",
+        "/waiting_room",
+        "/game_board",
+    ],
+)
+def test_get_with_cookie(player_client: TestClient, player_token: str, page_path: str):
+    """Authorized users should be able to access pages"""
+    assert isinstance(player_token, str)
+    response = player_client.get(page_path, follow_redirects=False)
+    assert response.status_code == status.HTTP_200_OK
