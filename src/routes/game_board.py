1bbb2e5a73282eee647f63cce7fb4828c7249e6f
RustyGuard <golovinartem2004@gmail.com>
2025-04-20 22:10:38 +0400

    Socket communication for turn info sending

--- src/routes/game_board.py
+++ src/routes/game_board.py
@@ -2,9 +2,11 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, Request, Response, WebSocket
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.attributes import flag_modified
 from sqlalchemy.sql import select
 
 from src.database import get_session
+from src.logic.game_process import make_a_turn
 from src.models import GameState
 from src.routes.auth import auth_required
 from src.templates import templates
@@ -27,13 +29,36 @@ def game_board_page(
     return templates.TemplateResponse(
         request=request,
         name="game_board.html",
-        context={},
+        context={
+            "state": state,
+            "settings_id": settings_id,
+        },
     )
 
 
-@game_board_router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+@game_board_router.websocket("/ws/{settings_id}")
+async def websocket_endpoint(
+    session: Annotated[Session, Depends(get_session)],
+    websocket: WebSocket,
+    settings_id: int,
+):
+    state: GameState | None = session.scalar(
+        select(GameState).where(GameState.settings_id == settings_id)
+    )
+    assert state is not None
     await websocket.accept()
     while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        data = await websocket.receive_json()
+        make_a_turn(state.holes_player1, state.holes_player2, int(data["hole"]))
+        flag_modified(state, "holes_player1")
+        flag_modified(state, "holes_player2")
+        session.commit()
+        print(data)
+        await websocket.send_json(
+            {
+                "type": "new_state",
+                "holes_player1": state.holes_player1,
+                "holes_player2": state.holes_player2,
+                "current_player": state.current_player,
+            }
+        )
